89ec4959c29ffa48f492fdf008bcbb84a6cf8b03
Trane <Trane_67@hotmail.de>
2024-07-15 01:08:45 +0200

    javascript start

---
+++ bestellapp/script.js
@@ -0,0 +1,31 @@
+let alldishes = [
+  {
+    salad: {
+      img: "./img/tst.png",
+      title: "Salat",
+      dishes: [
+        {
+          name: "Grüner Salat",
+          ingredients: "mit Gurken, Tomaten",
+          price: 6.0,
+        },
+        {
+          name: "Tomaten Salat",
+          ingredients: "mit Zwiebeln, Tomaten",
+          price: 6.0,
+        },
+      ],
+    },
+  },
+  {
+    pizza: {
+      img: "./img/tst.png",
+      title: "Pizza",
+      dishes: {
+        name: "PizzaRoma",
+        ingredients: "mit Schinken, Salami und Champignons",
+        price: 10.0,
+      },
+    },
+  },
+];
