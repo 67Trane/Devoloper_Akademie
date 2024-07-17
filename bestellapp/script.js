f7bf3a10450514b5f46910b7180341c85dab5609
Trane <Trane_67@hotmail.de>
2024-07-17 13:34:50 +0200

    update2

--- bestellapp/script.js
+++ bestellapp/script.js
@@ -1,7 +1,7 @@
 let alldishes = [
   {
     title: "Salat",
-    img: "./img/tst.png",
+    img: "./imgs/salad.png",
     dishes: [
       {
         name: "Grüner Salat",
@@ -15,16 +15,70 @@ let alldishes = [
         price: 6.0,
         img: "./asd.png",
       },
+      {
+        name: "Caesar Salat",
+        ingredients: "mit Hähnchen, Parmesan, Croutons",
+        price: 8.0,
+        img: "./asd.png",
+      },
     ],
   },
   {
     title: "Pizza",
-    img: "./img/tst.png",
+    img: "./imgs/pizza.jpg",
     dishes: [
       {
-        name: "PizzaRoma",
+        name: "Pizza Roma",
         ingredients: "mit Schinken, Salami und Champignons",
         price: 10.0,
+        img: "./asd.png",
+      },
+      {
+        name: "Pizza Margherita",
+        ingredients: "mit Tomatensauce und Mozzarella",
+        price: 8.0,
+        img: "./asd.png",
+      },
+      {
+        name: "Pizza Funghi",
+        ingredients: "mit Champignons und Mozzarella",
+        price: 9.0,
+        img: "./asd.png",
+      },
+    ],
+  },
+  {
+    title: "Pasta",
+    img: "./imgs/pasta.png",
+    dishes: [
+      {
+        name: "Spaghetti Bolognese",
+        ingredients: "mit Hackfleischsauce",
+        price: 12.0,
+        img: "./asd.png",
+      },
+      {
+        name: "Penne Arrabbiata",
+        ingredients: "mit scharfer Tomatensauce",
+        price: 10.0,
+        img: "./asd.png",
+      },
+    ],
+  },
+  {
+    title: "Desserts",
+    img: "./imgs/desserts.png",
+    dishes: [
+      {
+        name: "Tiramisu",
+        ingredients: "klassisches italienisches Dessert",
+        price: 6.0,
+        img: "./asd.png",
+      },
+      {
+        name: "Panna Cotta",
+        ingredients: "mit Beeren",
+        price: 5.0,
         img: "./asd.png",
       },
     ],
@@ -36,19 +90,22 @@ function render() {
 
   for (let i = 0; i < alldishes.length; i++) {
     content.innerHTML += `
-    <div class="dishesimg"></div>
+    <div id="dishesimg${i}"class="dishesimg"></div>
       <div class="dishestitle">
         <h2>${alldishes[i].title}</h2>
-      </div>
-                   `;
+      </div>`;
     renderdishes(i);
+    renderCategoryImg(i);
   }
+}
+
+function renderCategoryImg(i) {
+  let img = document.getElementById(`dishesimg${i}`);
+  img.style.backgroundImage = `url(${alldishes[i].img})`;
 }
 
 function renderdishes(j) {
   let dishes = document.getElementById(`dishes`);
-  console.log(dishes);
-
   for (let i = 0; i < alldishes[j].dishes.length; i++) {
     dishes.innerHTML += `
     <div id="dish${i}" class="dish">
@@ -56,13 +113,64 @@ function renderdishes(j) {
             <h2>${alldishes[j].dishes[i].name}</h2>
             <p>${alldishes[j].dishes[i].ingredients}</p>
             <h2>${parseFloat(alldishes[j].dishes[i].price)
-                .toFixed(2)
-                .replace(".", ",")}€</h2>
-            
+              .toFixed(2)
+              .replace(".", ",")}€</h2>
         </div>
-        <div class="addbtn" id="addbtn${j}">
+        <div class="addbtn" id="addbtn${j}${i}" onclick="addbasket(${j}, ${i})">
                 <button></button>
             </div>
         </div>`;
   }
+}
+
+function addbasket(j, i) {
+  let clicked = 0
+  
+  if (clicked == 0) {
+    alert("test")
+    clicked = 2
+  } 
+
+  console.log(clicked)
+  let basket = document.getElementById("basket-section")
+  switchBasketInfo();
+  let amount = 1;
+
+  basket.innerHTML += renderBasketInfos(j, i, amount);
+ 
+
+}
+
+function renderBasketInfos(j, i, amount) {
+  return ` <div id="basket-container" class="basket-container">
+                <div class="basket-infos">
+                    <div class="amount-price">
+                        <div class="amount-name">
+                            <h3>${amount}</h3>
+                            <h3 class="dishname">${alldishes[j].dishes[i].name}</h3>
+                        </div>
+                        <h3 class="price">${parseFloat(alldishes[j].dishes[i].price)
+                          .toFixed(2)
+                          .replace(".", ",")}€</h3>
+                    </div>
+                    <div class="ingridients">
+                        <p>${alldishes[j].dishes[i].ingredients}</p>
+                    </div>
+
+                    <div class="add-amount">
+                        <a href="#">Anmerkung<br> hinzufügen</a>
+                        <div class="add-remove">
+                            <button class="remove"></button>
+                            <h3>${amount}</h3>
+                            <button class="add"></button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>`;
+}
+
+function switchBasketInfo() {
+  let basket = document.getElementById("basketempty");
+  basket.classList.add("d-none");
 }
