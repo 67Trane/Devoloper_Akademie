14d07cbae38a1b7e776ef380cfc62cfe853c7475
67Trane <144054852+67Trane@users.noreply.github.com>
2024-07-22 17:49:18 +0000

    update1

--- Firma/javascript/array.js
+++ Firma/javascript/array.js
@@ -4,29 +4,34 @@ let maschines = [
     name: "SM -",
     text: "",
     selected: false,
+    color: ""
   },
   {
     id: "maschine2",
     name: "SM 12",
     text: "",
     selected: false,
+    color: ""
   },
   {
     id: "maschine3",
     name: "SM 11",
     text: "",
     selected: false,
+    color: ""
   },
   {
     id: "maschine4",
     name: "SM 10",
     text: "",
     selected: false,
+    color: ""
   },
   {
     id: "maschine5",
     name: "SM 9",
     text: "",
     selected: false,
+    color: ""
   },
 ];
--- Firma/script.js
+++ Firma/script.js
@@ -1,13 +1,12 @@
 let selectedmaschineid = 0;
 
 function loaded() {
-  load()
+  load();
 }
 
 function selectmaschine(id) {
-  console.log(id)
-  renderMaschineHeader(id) 
-  allselectedOff()
+  renderMaschineHeader(id);
+  allselectedOff();
   maschines[id].selected = true;
   for (let i = 0; i < maschines.length; i++) {
     let selected = maschines[i].selected;
@@ -15,44 +14,64 @@ function selectmaschine(id) {
       selectedmaschineid = id;
     }
   }
-  clearTextField()
-  renderTextfield(id)
+  clearTextField();
+  renderTextfield(id);
+  colorMaschine(id);
 }
 
 function textfield() {
   let text = document.getElementById("textarea").value;
   maschines[selectedmaschineid].text = text;
-  save()
+  save();
 }
 
-function allselectedOff(){
+function allselectedOff() {
   for (let i = 0; i < maschines.length; i++) {
-    maschines[i].selected = false
+    maschines[i].selected = false;
   }
 }
 
 function clearTextField() {
-  let textfield = document.getElementById("textarea")
-  textfield.value = ""
+  let textfield = document.getElementById("textarea");
+  textfield.value = "";
 }
 
 function renderMaschineHeader(id) {
-  let header = document.getElementById("maschineheader")
-  header.innerHTML = maschines[id].name
+  let header = document.getElementById("maschineheader");
+  header.innerHTML = maschines[id].name;
 }
 
 function save() {
-  localStorage.setItem("maschines", JSON.stringify(maschines))
+  localStorage.setItem("maschines", JSON.stringify(maschines));
 }
 
 function load() {
-  let maschinesinfos = localStorage.getItem("maschines")
-  let parsedinfos = JSON.parse(maschinesinfos)
-  maschines = parsedinfos
+  let maschinesinfos = localStorage.getItem("maschines");
+  let parsedinfos = JSON.parse(maschinesinfos);
+  maschines = parsedinfos;
 }
 
 function renderTextfield(id) {
-  let text = maschines[id].text
-  let textfield = document.getElementById("textarea")
-  textfield.value = text
+  let text = maschines[id].text;
+  let textfield = document.getElementById("textarea");
+  textfield.value = text;
+}
+
+/* Checkbox Bereich */
+
+function checkbox(colour) {
+  let green = document.getElementById("green");
+  let yellow = document.getElementById("yellow");
+  let red = document.getElementById("red");
+
+  green.checked = false;
+  yellow.checked = false;
+  red.checked = false;
+
+  colour.checked = true;
+}
+
+function colorMaschine(id) {
+  let maschine = document.getElementById(`maschine${id + 1}`);
+  maschine.style.backgroundColor = "rgb(201, 194, 194)";
 }
