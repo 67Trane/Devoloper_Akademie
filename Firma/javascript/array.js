a023dd97588716fecbb626a3a2dcfe6ed1d8ec3f
Trane <Trane_67@hotmail.de>
2024-07-24 00:02:19 +0200

    array

--- Firma/javascript/array.js
+++ Firma/javascript/array.js
@@ -34,4 +34,207 @@ let maschines = [
     selected: false,
     color: ""
   },
+  {
+    id: "maschine6",
+    name: "SM 8",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine7",
+    name: "SM 101",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine8",
+    name: "SM 22",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine9",
+    name: "SM 5",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine10",
+    name: "SM 4",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine11",
+    name: "SM 3",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine12",
+    name: "SM 2",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine13",
+    name: "SM 14",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine14",
+    name: "SM 14",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine15",
+    name: "SM 119",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine16",
+    name: "SM 138",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine17",
+    name: "SM 146",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine18",
+    name: "SM 1",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine19",
+    name: "SM 1",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine20",
+    name: "SM 1",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine21",
+    name: "SM 106",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine22",
+    name: "SM 6",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine23",
+    name: "SM 13",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine24",
+    name: "SM 15",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine25",
+    name: "SM 7",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine26",
+    name: "SM 17",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine27",
+    name: "SM 18",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine28",
+    name: "SM 19",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine29",
+    name: "SM 50",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine30",
+    name: "SM 21",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine31",
+    name: "SM 94",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine32",
+    name: "SM 115",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine33",
+    name: "SM 143",
+    text: "",
+    selected: false,
+    color: ""
+  },
+  {
+    id: "maschine34",
+    name: "SM -",
+    text: "",
+    selected: false,
+    color: ""
+  }
 ];
