f31185803620b638e86b0a167c3f7e49e0ee4d7b
Trane <Trane_67@hotmail.de>
2024-08-06 00:09:30 +0200

    pokedex update

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -1,12 +1,14 @@
 const POKEDEX_API = "https://pokeapi.co/api/v2";
 
+let pokemonCount = 0;
+
 function init() {
   loadingScreen();
   loadPokemons();
 }
 
 function loadingScreen() {
-  document.getElementById("test").style.backgroundColor = "red";
+  document.getElementById("loading-screen").classList.remove("d-none");
 }
 
 async function loadPokemons() {
@@ -14,8 +16,7 @@ async function loadPokemons() {
     .then((response) => response.json())
     .then((data) => renderPokemonCard(data.results))
     .then(
-      () =>
-        (document.getElementById("test").style.backgroundColor = "green")
+      () => (document.getElementById("loading-screen").classList.add("d-none"))
     )
     .catch((error) => console.error(error));
 }
@@ -37,28 +38,25 @@ function pokemon(pokemon, i) {
 }
 
 function renderPokemonType(pokemon, i) {
+    document.getElementById(`card${i}`).classList.add(pokemon.types[0].type.name+ "-bg")
+
   for (let z = 0; z < pokemon.types.length; z++) {
     let type = pokemon.types[z].type.name;
-    document.getElementById(
-      `type${i}`
-    ).innerHTML += `<span class="badge ${type}">${pokemon.types[z].type.name}</span>`;
+    document.getElementById(`type${i}`).innerHTML += `<span class="badge ${type}">${pokemon.types[z].type.name}</span>`;
+    
   }
 }
 
 function renderPokemonCard(AllPokemons) {
-  for (let i = 0; i < AllPokemons.length; i++) {
+  for (let i = pokemonCount; i < pokemonCount + AllPokemons.length; i++) {
     loadPokemon(i + 1);
-    let Pokemon = AllPokemons[i];
+    let Pokemon = AllPokemons[i - pokemonCount];
     let pokemonContainer = document.getElementById("pokemon-container");
-    pokemonContainer.innerHTML += `<div class="card" id="card" style="width: 18rem;" onclick="openPokemonCard(${i})">
-                    <img src="icon/1.png" class="card-img-top" alt="..." id="pokemonpicture${
-                      i + 1
-                    }">
+    pokemonContainer.innerHTML += `<div class="card shadow" id="card${i+1}" style="width: 18rem;" onclick="openPokemonCard(${i+1})">
+                    <img src="#" class="card-img-top" alt="..." id="pokemonpicture${i + 1}">
                     <div class="card-body">
                         <div class="pokemon-info-text">
-                            <p class="card-text" id="pokemon-id${
-                              i + 1
-                            }">Nr. </p>
+                            <p class="card-text" id="pokemon-id${i + 1}">Nr. </p>
                             <h5 class="card-title">${Pokemon.name.toUpperCase()}</h5>
                         </div>
                         <div class="type" id="type${i + 1}">
@@ -66,8 +64,20 @@ function renderPokemonCard(AllPokemons) {
                     </div>
                 </div>`;
   }
+  pokemonCount += AllPokemons.length;
 }
 
 function openPokemonCard(i) {
-  console.log(i);
+  document.getElementById("fullscreen-container").classList.remove("d-none")
+}
+
+async function loadMore() {
+  fetch(`${POKEDEX_API}/pokemon?limit=20&offset=${pokemonCount}`)
+    .then((response) => response.json())
+    .then((data) => renderPokemonCard(data.results, pokemonCount))
+    .catch((error) => console.error(error));
+}
+
+function renderFullscreenPokemon() {
+
 }
