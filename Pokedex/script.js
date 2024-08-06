27df30f6f56828df5dd7d181071697801be22b05
Trane <Trane_67@hotmail.de>
2024-08-06 18:29:11 +0200

    pokedex update

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -12,38 +12,44 @@ function loadingScreen() {
 }
 
 async function loadPokemons() {
+  loadingScreen();
   fetch(POKEDEX_API + "/pokemon?limit=20&offset=0")
     .then((response) => response.json())
     .then((data) => renderPokemonCard(data.results))
-    .then(
-      () => (document.getElementById("loading-screen").classList.add("d-none"))
+    .then(() =>
+      document.getElementById("loading-screen").classList.add("d-none")
     )
     .catch((error) => console.error(error));
 }
 
 async function loadPokemon(number) {
+  loadingScreen();
   fetch(POKEDEX_API + "/pokemon/" + number)
     .then((response) => response.json())
     .then((data) => pokemon(data, number))
+    .then(() => document.getElementById("loading-screen").classList.add("d-none"))
     .catch((error) => console.error(error));
 }
 
 function pokemon(pokemon, i) {
+
   let pokemonId = pokemon.id;
   document.getElementById(`pokemon-id${i}`).innerHTML += pokemonId;
-  document.getElementById(
-    `pokemonpicture${i}`
-  ).src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+  document.getElementById(`pokemonpicture${i}`).src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
   renderPokemonType(pokemon, i);
+
 }
 
 function renderPokemonType(pokemon, i) {
-    document.getElementById(`card${i}`).classList.add(pokemon.types[0].type.name+ "-bg")
+  document
+    .getElementById(`card${i}`)
+    .classList.add(pokemon.types[0].type.name + "-bg");
 
   for (let z = 0; z < pokemon.types.length; z++) {
     let type = pokemon.types[z].type.name;
-    document.getElementById(`type${i}`).innerHTML += `<span class="badge ${type}">${pokemon.types[z].type.name}</span>`;
-    
+    document.getElementById(
+      `type${i}`
+    ).innerHTML += `<span class="badge ${type}">${pokemon.types[z].type.name}</span>`;
   }
 }
 
@@ -52,11 +58,17 @@ function renderPokemonCard(AllPokemons) {
     loadPokemon(i + 1);
     let Pokemon = AllPokemons[i - pokemonCount];
     let pokemonContainer = document.getElementById("pokemon-container");
-    pokemonContainer.innerHTML += `<div class="card shadow" id="card${i+1}" style="width: 18rem;" onclick="openPokemonCard(${i+1})">
-                    <img src="#" class="card-img-top" alt="..." id="pokemonpicture${i + 1}">
+    pokemonContainer.innerHTML += `<div class="card shadow" id="card${
+      i + 1
+    }" style="width: 18rem;" onclick="openPokemonCard(${i + 1})">
+                    <img src="#" class="card-img-top" alt="..." id="pokemonpicture${
+                      i + 1
+                    }">
                     <div class="card-body">
                         <div class="pokemon-info-text">
-                            <p class="card-text" id="pokemon-id${i + 1}">Nr. </p>
+                            <p class="card-text" id="pokemon-id${
+                              i + 1
+                            }">Nr. </p>
                             <h5 class="card-title">${Pokemon.name.toUpperCase()}</h5>
                         </div>
                         <div class="type" id="type${i + 1}">
@@ -67,17 +79,23 @@ function renderPokemonCard(AllPokemons) {
   pokemonCount += AllPokemons.length;
 }
 
-function openPokemonCard(i) {
-  document.getElementById("fullscreen-container").classList.remove("d-none")
-}
-
 async function loadMore() {
+  loadingScreen();
   fetch(`${POKEDEX_API}/pokemon?limit=20&offset=${pokemonCount}`)
     .then((response) => response.json())
     .then((data) => renderPokemonCard(data.results, pokemonCount))
+    .then(() => document.getElementById("loading-screen").classList.add("d-none"))
     .catch((error) => console.error(error));
 }
 
-function renderFullscreenPokemon() {
-
+function openPokemonCard(i) {
+  document.getElementById("fullscreen-container").classList.remove("d-none");
+  document.getElementById("body").style.overflowY = "hidden";
 }
+
+function closeFullscreen() {
+  document.getElementById("fullscreen-container").classList.add("d-none");
+  document.getElementById("body").style.overflowY = "auto";
+}
+
+function renderFullscreenPokemon(id, name) {}
